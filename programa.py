56fbcea5e70be48f9732764b35b311db41341e5e
salacp12 <135990712+salacp12@users.noreply.github.com>
2023-06-22 12:30:55 +0000

    Jisela

---
+++ programa.py
@@ -0,0 +1,8 @@
+from matematicas import sumar, restar, multiplicar, dividir
+from algebra import distancia
+
+print(sumar(4, 5))
+print(restar(4, 5))
+print(multiplicar(4, 5))
+print(dividir(4, 5))
+print(distancia(1, 2, 3, 4))
